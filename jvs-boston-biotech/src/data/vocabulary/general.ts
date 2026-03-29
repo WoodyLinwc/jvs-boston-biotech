b0cc357afbca0011c80766c8788cd5ea4f70ce87
Woody Lin <wancheng.lin001@gmail.com>
2026-03-29 11:43:14 -0400

    lab supplies

--- jvs-boston-biotech/src/data/vocabulary/general.ts
+++ jvs-boston-biotech/src/data/vocabulary/general.ts
@@ -1,3 +1,50 @@
 import { VocabularyTerm } from "../types";
 
-export const generalEn: VocabularyTerm[] = [];
+export const generalEn: VocabularyTerm[] = [
+  {
+    term: "Sterilization",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "Vented",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "PCR (Polymerase Chain Reaction)",
+    definition:
+      "It's a technique used to amplify (make many copies of) a specific segment of DNA",
+    imageUrl: "",
+  },
+  {
+    term: "Eukaryotes",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "E. coli",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "",
+    definition: "",
+    imageUrl: "",
+  },
+  {
+    term: "",
+    definition: "",
+    imageUrl: "",
+  },
+];
--- jvs-boston-biotech/src/data/vocabulary/lab-supplies.ts
+++ jvs-boston-biotech/src/data/vocabulary/lab-supplies.ts
@@ -1,9 +1,148 @@
 import { VocabularyTerm } from "../types";
 
 export const labSuppliesEn: VocabularyTerm[] = [
+  // {
+  //   term: "",
+  //   definition: "",
+  //   imageUrl: "",
+  // },
   {
-    term: "Pipette",
+    term: "Erlenmeyer flask",
     definition:
-      "A laboratory tool used to transport a measured volume of liquid. Available in single-channel and multi-channel formats for various throughput needs.",
+      "Used for mixing and heating liquids without spilling due to its narrow neck, also cell growth, inaccurate measurement",
+    imageUrl: "lab-supplies/Erlenmeyer-flask.jpeg",
   },
+  {
+    term: "Beaker",
+    definition:
+      "A general-purpose container for mixing and heating liquids and solids, inaccurate measurement",
+    imageUrl: "lab-supplies/Beaker.jpeg",
+  },
+  {
+    term: "Test tube",
+    definition:
+      "Used to hold small amounts of liquid for reactions, inaccurate measurement",
+    imageUrl: "lab-supplies/Test-tube.jpeg",
+  },
+  {
+    term: "Volumetric flask",
+    definition:
+      "Designed to prepare and measure a precise volume of solution. (e.g., exactly 100 mL or 250 mL)",
+    imageUrl: "lab-supplies/Volumetric-flask.jpeg",
+  },
+  {
+    term: "Graduated cylinder",
+    definition: "Used to accurately measure the volume of a liquid",
+    imageUrl: "lab-supplies/Graduated-cylinder.jpeg",
+  },
+  {
+    term: "PCR tubes",
+    definition:
+      "Designed to hold DNA/RNA samples during PCR amplification in a thermocycler",
+    imageUrl: "lab-supplies/PCR-tubes.jpeg",
+  },
+  {
+    term: "Microfuge tubes (Eppendorf)",
+    definition:
+      "Small tubes (typically 1.5–2 mL) used to store, mix, or centrifuge small volumes of samples",
+    imageUrl: "lab-supplies/Microfuge-tubes.jpeg",
+  },
+  {
+    term: "96 Well Plates",
+    definition:
+      "Flat plates with 96 small wells used for cell culture or screening experiments, high-throughput assays (the ability to test hundreds or thousands of samples simultaneously in a fast, automated way)",
+    imageUrl: "lab-supplies/96-Well-Plates.jpeg",
+  },
+  {
+    term: "Sterile Petri Plates",
+    definition:
+      "Shallow dishes used to grow cells on solid agar media, grow micro organism",
+    imageUrl: "lab-supplies/Sterile-Petri-Plates.jpeg",
+  },
+  {
+    term: "Centrifuge tubes",
+    definition:
+      "Larger tubes (e.g., 15 mL or 50 mL) designed to withstand the force of centrifugation for separating samples",
+    imageUrl: "lab-supplies/Centrifuge-tubes.jpeg",
+  },
+  {
+    term: "T-Flasks",
+    definition:
+      "Flat-bottomed flasks used to grow adherent mammalian cells on their surface in cell culture",
+    imageUrl: "lab-supplies/T-Flasks.jpeg",
+  },
+  {
+    term: "Parafilm",
+    definition:
+      "Waxy film used to seal containers and prevent evaporation or contamination, heat sensitive",
+    imageUrl: "lab-supplies/Parafilm.jpeg",
+  },
+  {
+    term: "Latex (Natural Rubber)",
+    definition:
+      "Good protection against biological materials (blood, bacteria, cells), but can cause latex allergies",
+    imageUrl: "lab-supplies/Latex.jpeg",
+  },
+  {
+    term: "Nitrile (Synthetic Rubber)",
+    definition:
+      "Currently the most popular glove in biotech/medical labs, slightly more expensive than vinyl",
+    imageUrl: "lab-supplies/Nitrile.jpeg",
+  },
+  {
+    term: "Vinyl (PVC)",
+    definition:
+      "Cheapest, good for low-risk tasks — handling non-hazardous materials, food handling, light cleaning",
+    imageUrl: "lab-supplies/Vinyl.jpeg",
+  },
+  {
+    term: "Pipettes",
+    definition:
+      "Used to transfer liquids from one container to another in measured volumes",
+    imageUrl: "lab-supplies/Pipettes.jpeg",
+  },
+  {
+    term: "Micropipettes",
+    definition:
+      "Used to measure and transfer very small, precise volumes, 0.1 µL to 1000 µL",
+    imageUrl: "lab-supplies/Micropipettes.jpeg",
+  },
+  {
+    term: "Tips",
+    definition:
+      "Disposable plastic tips that attach to the end of micropipettes",
+    imageUrl: "lab-supplies/Tips.jpeg",
+  },
+  {
+    term: "Transfer pipettes",
+    definition:
+      "Simple, disposable plastic pipettes, non-precise transfers of liquids",
+    imageUrl: "lab-supplies/Transfer-pipettes.jpeg",
+  },
+  {
+    term: "Bunsen Burner",
+    definition:
+      "A gas-powered flame device for creating a sterile working environment",
+    imageUrl: "lab-supplies/Bunsen-Burner.jpeg",
+  },
+  {
+    term: "Stirrer/Hot Plate",
+    definition: "A combined instrument that can heat AND stir at the same time",
+    imageUrl: "lab-supplies/Stirrer.jpeg",
+  },
+  {
+    term: "Magnetic Stir Bar (Stir Flea)",
+    definition:
+      "A small magnet coated in Teflon (PTFE) placed inside the liquid in a container",
+    imageUrl: "lab-supplies/Magnetic-Stir-Bar.jpeg",
+  },
+  {
+    term: "Water Purification",
+    definition: "Filtered water does not contain minerals, NOT sterile",
+  },
+  // {
+  //   term: "",
+  //   definition: "",
+  //   imageUrl: "",
+  // },
 ];
